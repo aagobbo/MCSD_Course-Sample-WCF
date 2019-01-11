6b9fee85626af751021fbdc32f8372f6f68b044b
Adriano Antonio Gobbo <gobbo.email@gmail.com>
2019-01-11 16:20:34 -0200

    WCF course MCSD

---
+++ WcfSample/WcfClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var service = new WcfClient.CustomService.Service1Client())
+            {
+                service.Open();
+
+                var data = service.GetData(10);
+                Console.WriteLine("data " + data);
+
+                var compositeType = new CustomService.CompositeType();
+                compositeType.BoolValue = true;
+                compositeType.StringValue = "teste";
+
+                var composite = service.GetDataUsingDataContract(compositeType);
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ WcfSample/WcfClient2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfSample;
+
+namespace WcfClient2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var channel = new ChannelFactory<IService1>(new BasicHttpBinding(), "http://localhost:54896/Service1.svc"))
+            {
+                channel.Open();
+
+                var service = channel.CreateChannel();
+
+                var resultado = service.GetData(11);
+
+                Console.WriteLine(resultado);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
